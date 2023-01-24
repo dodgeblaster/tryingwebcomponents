77a4eda3bb73225e5823b56084fd588a43a41315
Gary Jennings <garysjennings@gmail.com>
2023-01-24 10:28:04 -0700

    first commit

---
+++ base/Button.js
@@ -0,0 +1,28 @@
+class Button extends HTMLElement {
+    constructor() {
+        super()
+        const name = this.getAttribute('name')
+        const shadow = this.attachShadow({ mode: 'open' })
+        shadow.innerHTML = /*html*/ `
+            <link rel="stylesheet" href="./style.css" />
+            <button
+                class="px-4 py-1 mr-2 text-sm rounded-md bg-gradient-to-tr from-violet-500 to-fuchsia-500 text-white"
+            >
+                ${name}
+            </button>
+        `
+    }
+
+    static get observedAttributes() {
+        return ['name']
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'name') {
+            const button = this.shadowRoot.querySelector('button')
+            button.textContent = newValue
+        }
+    }
+}
+
+customElements.define('gj-button', Button)
---
+++ sections/Header.js
@@ -0,0 +1,31 @@
+class Header extends HTMLElement {
+    state = {
+        count: 0
+    }
+
+    constructor() {
+        super()
+        this.shadow = this.attachShadow({ mode: 'open' })
+        this.shadow.innerHTML = /*html*/ `
+            <link rel="stylesheet" href="./style.css" />
+            <div class='w-full h-10 bg-gray-800 px-4 flex items-center'>
+                <gj-button id='login' name="Login"></gj-button>
+                <gj-button id='other' name="Other ${this.state.count}"></gj-button>
+            </div>
+        `
+    }
+
+    connectedCallback() {
+        this.shadow.querySelector('#login').addEventListener('click', () => {
+            alert('clicked')
+        })
+
+        const otherButton = this.shadow.querySelector('#other')
+        otherButton.addEventListener('click', () => {
+            this.state.count = this.state.count + 1
+            otherButton.setAttribute('name', `Other ${this.state.count}`)
+        })
+    }
+}
+
+customElements.define('gj-header', Header)
